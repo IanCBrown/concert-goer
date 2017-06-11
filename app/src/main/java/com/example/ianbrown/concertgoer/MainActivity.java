d1e8478b5c103673aa2e1abc6b1a090bc15d13db
Ian Brown <icb0004@auburn.edu>
2017-06-10 23:59:10 -0500

    Closer to dynamic ListView functionality

--- app/src/main/java/com/example/ianbrown/concertgoer/MainActivity.java
+++ app/src/main/java/com/example/ianbrown/concertgoer/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.ianbrown.concertgoer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    public String[] defaultBandNames = {"Band", "names", "would", "go", "here"};
+    public String[] userBandNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) findViewById(R.id.idListView);
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, defaultBandNames);
+        lv.setAdapter(adapter);
+
     }
 }
