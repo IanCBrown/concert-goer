2797a467683e9db43c1d8ff2b0db0c6abc10a395
Ian Brown <icb0004@auburn.edu>
2017-07-29 21:57:27 -0500

    Added floating context menu with edit (WIP), delete, and delete all options. Locked orientation to portrait for the time being.

--- app/src/main/java/com/example/ianbrown/concertgoer/MainActivity.java
+++ app/src/main/java/com/example/ianbrown/concertgoer/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.ianbrown.concertgoer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.KeyEvent;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lv = (ListView) findViewById(R.id.listView);
-
+        registerForContextMenu(lv);
 
         arrayList = new ArrayList<>(Arrays.asList(defaultBandNames));
 
@@ -73,5 +78,46 @@ public class MainActivity extends AppCompatActivity {
             adapter.add(newBand);
             txtInput.getText().clear();
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.edit:
+                //edit();
+                Toast.makeText(this, "Placeholder", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete:
+                delete(info);
+                Toast.makeText(this, R.string.item_removed, Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.delete_all:
+                deleteAll();
+                Toast.makeText(this, R.string.list_cleared, Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    public void delete(AdapterView.AdapterContextMenuInfo info) {
+        adapter.remove(adapter.getItem(info.position));
+    }
+
+    public void deleteAll() {
+        adapter.clear();
+    }
+
+    public void edit(AdapterView.AdapterContextMenuInfo info) {
+        //TODO add proper edit functionality
     }
 }
