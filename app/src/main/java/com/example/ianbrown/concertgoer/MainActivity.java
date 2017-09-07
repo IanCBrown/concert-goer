b649ce051df839f8cc37b6d9bf79a6aa87f285a2
Ian Brown <icb0004@auburn.edu>
2017-09-07 15:48:15 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/ianbrown/concertgoer/MainActivity.java
+++ app/src/main/java/com/example/ianbrown/concertgoer/MainActivity.java
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //sketchy hard coding incoming, don't cringe
                 ArrayList<String> defaultBandNamesList = new ArrayList<>(Arrays.asList(defaultBandNames));
                 addBandsToList(defaultBandNamesList, txtInput);
              }
@@ -58,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
         txtInput.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                //sketchy hard coding incoming, don't cringe
                 ArrayList<String> defaultBandNamesList = new ArrayList<>(Arrays.asList(defaultBandNames));
                 if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                     addBandsToList(defaultBandNamesList, txtInput);
